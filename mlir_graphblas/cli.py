1d697dbb29b6f87340dd96e80008d99725cb089f
Jim Kitchen <jim22k@gmail.com>
2021-02-25 18:04:23 -0600

    Add `explore` method to debug results

--- mlir_graphblas/cli.py
+++ mlir_graphblas/cli.py
@@ -2,6 +2,7 @@ import os
 import math
 import subprocess
 import tempfile
+from functools import partial
 from typing import List, Optional, Union
 
 
@@ -50,7 +51,7 @@ class MlirOptCli:
         stages = []
         saved_passes = []
         for p in passes:
-            saved_passes.append(p)
+            saved_passes.append(p.lstrip('-'))
             stages.append(input.decode())
             result = subprocess.run([self._executable, p], capture_output=True, input=input)
             if result.returncode == 0:
@@ -75,7 +76,7 @@ class DebugResult:
         for p, stage in zip(reversed(self.passes), reversed(self.stages[:-1])):
             if stage == self.stages[-2]:
                 stage = self._add_row_column_numbers(stage)
-            ret.append(self._add_banner(stage, f"Input to {p[2:]}"))
+            ret.append(self._add_banner(stage, f"Input to {p}"))
         return "\n\n".join(ret)
 
     def __getitem__(self, item):
@@ -92,6 +93,13 @@ class DebugResult:
             trim_passes = self.passes[item.start:item.stop-1]
         return DebugResult(trim_stages, trim_passes)
 
+    def _find_pass_index(self, pass_):
+        for ip, p in enumerate(self.passes):
+            if p == pass_:
+                return ip
+        else:
+            raise KeyError(f"No pass found named {pass_}")
+
     def diff(self, pass_, diffcmd=None):
         """
         Performs a diff showing the input and output of a given `pass_`.
@@ -102,20 +110,15 @@ class DebugResult:
             from . import config
             diffcmd = config.get("diff.executable", "vimdiff")
 
-        # Find pass_
-        for ip, p in enumerate(self.passes):
-            if p == pass_:
-                break
-        else:
-            raise ValueError(f"No pass found named {pass_}")
+        ipass = self._find_pass_index(pass_)
 
         with tempfile.TemporaryDirectory() as tmpdir:
             file1 = os.path.join(tmpdir, "first.txt")
             file2 = os.path.join(tmpdir, "second.txt")
             with open(file1, "w") as f:
-                f.write(self.stages[ip])
+                f.write(self.stages[ipass])
             with open(file2, "w") as f:
-                f.write(self.stages[ip+1])
+                f.write(self.stages[ipass + 1])
             subprocess.run([diffcmd, file1, file2])
 
     @classmethod
@@ -145,3 +148,110 @@ class DebugResult:
         for i, line in enumerate(splitz, 1):
             ret.append(f'{i:{offset}}|{line}')
         return "\n".join(ret)
+
+    def explore(self):
+        import panel as pn
+        pn.extension()
+
+        tabs = pn.Tabs()
+
+        # Single
+        single = pn.Column(
+            pn.widgets.Select(name='Passes', options=['Initial'] + self.passes),
+            pn.pane.HTML(code_block.replace('{{textarea}}', self.stages[0]).replace('{{id}}', 'foobar_single')),
+        )
+        tabs.append(('Single', single))
+
+        # Double
+        double = pn.Row(
+            pn.Column(
+                pn.widgets.Select(name='Passes', options=['Initial'] + self.passes),
+                pn.pane.HTML(code_block.replace('{{textarea}}', self.stages[0]).replace('{{id}}', 'foobar_double1')),
+            ),
+            pn.Column(
+                pn.widgets.Select(name='Passes', options=['Initial'] + self.passes, value=self.passes[0]),
+                pn.pane.HTML(code_block.replace('{{textarea}}', self.stages[1]).replace('{{id}}', 'foobar_double2')),
+            )
+        )
+        tabs.append(('Double', double))
+
+        # Sequential
+        sequential = pn.Column(
+            pn.widgets.Select(name='Passes', options=self.passes),
+            pn.Row(
+                pn.widgets.Button(name="<--"),
+                pn.widgets.Button(name="-->"),
+            ),
+            pn.Row(
+                pn.pane.HTML(code_block.replace('{{textarea}}', self.stages[0]).replace('{{id}}', 'foobar_seq1')),
+                pn.pane.HTML(code_block.replace('{{textarea}}', self.stages[1]).replace('{{id}}', 'foobar_seq2')),
+            )
+        )
+        tabs.append(('Sequential', sequential))
+
+        # Callbacks
+        def code_callback(target, event, offset=0):
+            if event.new == "Initial":
+                new_text = self.stages[0 + offset]
+            else:
+                try:
+                    ipass = self._find_pass_index(event.new)
+                    new_text = self.stages[ipass + 1 + offset]
+                except KeyError:
+                    new_text = f"No pass found named {event.new}"
+
+            a = target.object.index("<textarea id=")
+            b = target.object.index(">", a) + 2
+            c = target.object.index("\n</textarea", b)
+            target.object = target.object[:b] + new_text + target.object[c:]
+
+        def button_callback(target, event):
+            ipass = self._find_pass_index(target.value)
+            if event.obj.name == "<--":
+                target.value = self.passes[max(ipass-1, 0)]
+            elif event.obj.name == "-->":
+                target.value = self.passes[min(ipass+1, len(self.passes)-1)]
+
+        single[0].link(single[1], callbacks={'value': code_callback})
+        double[0][0].link(double[0][1], callbacks={'value': code_callback})
+        double[1][0].link(double[1][1], callbacks={'value': code_callback})
+        sequential[0].link(sequential[2][0], callbacks={'value': partial(code_callback, offset=-1)})
+        sequential[0].link(sequential[2][1], callbacks={'value': code_callback})
+        sequential[1][0].link(sequential[0], callbacks={'value': button_callback})
+        sequential[1][1].link(sequential[0], callbacks={'value': button_callback})
+
+        return tabs
+
+
+code_block = '''
+<html>
+<head>
+<script type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.52.2/codemirror.min.js">
+</script>
+
+<script type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.52.2/mode/javascript/javascript.min.js">
+</script>
+</head>
+<body>
+
+<div>
+<textarea id="{{id}}">
+{{textarea}}
+</textarea>
+</div>
+
+<script>
+var config = {
+  lineNumbers: true,
+  tabSize: 2,
+  readOnly: true,
+  mode: 'javascript',
+};
+var myTextArea = document.querySelector('#{{id}}');
+var cm = CodeMirror.fromTextArea(myTextArea, config);
+</script>
+</body>
+</html>
+'''
