a9eafa17b06c3c38bb985e780bd524e08f54e517
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-03-05 10:11:45 -0800

    JIT Engine to handle (fixed_shape_tensor, scalar) -> fixed_shape_tensor ; add tests
    This commit let's the JIT engine handle MLIR functions that take a fixed size tensor and  a scalar and return a fixed size tensor.
    
    This commit also adds tests for the JIT engine.

---
+++ mlir_graphblas/tests/test_jit_engine.py
@@ -0,0 +1,198 @@
+
+import mlir_graphblas
+import pytest
+import numpy as np
+
+###########
+# Globals #
+###########
+
+STANDARD_PASSES = [
+    '--linalg-bufferize',
+    '--func-bufferize',
+    '--finalizing-bufferize',
+    '--convert-linalg-to-loops',
+    '--convert-scf-to-std',
+    '--convert-std-to-llvm',
+]
+
+MLIR_TYPE_TO_NP_TYPE = {
+    'i8': np.int8,
+    'i16': np.int16,
+    'i32': np.int32,
+    'i64': np.int64,
+    # 'f16': np.float16, # 16-bit floats don't seem to be supported in ctypes
+    'f32': np.float32,
+    'f64': np.float64,
+}
+
+TEST_CASES = [ # elements are ( mlir_template, args, expected_result x)
+    
+    ( # fixed size tensor + fixed size tensor -> fixed size tensor
+        """
+#trait_add = {{
+ indexing_maps = [
+   affine_map<(i, j) -> (i, j)>,
+   affine_map<(i, j) -> (i, j)>,
+   affine_map<(i, j) -> (i, j)>
+ ],
+ iterator_types = [\"parallel\", \"parallel\"],
+ doc = \" !!! DUMMY DOCUMENTATION !!! \"
+}}
+func @{func_name}(%arga: tensor<2x4x{mlir_type}>, %argb: tensor<2x4x{mlir_type}>) -> tensor<2x4x{mlir_type}> {{
+ %answer = linalg.generic #trait_add
+    ins(%arga, %argb: tensor<2x4x{mlir_type}>, tensor<2x4x{mlir_type}>)
+   outs(%arga: tensor<2x4x{mlir_type}>) {{
+     ^bb(%a: {mlir_type}, %b: {mlir_type}, %s: {mlir_type}):
+       %sum = {linalg_add} %a, %b : {mlir_type}
+       linalg.yield %sum : {mlir_type}
+ }} -> tensor<2x4x{mlir_type}>
+ return %answer : tensor<2x4x{mlir_type}>
+}}
+""",
+        [
+            np.arange(2*4).reshape((2,4)),
+            np.full([2,4], 10),
+        ],
+        np.array([
+            [10, 11, 12, 13],
+            [14, 15, 16, 17]
+        ])
+    ),
+    
+    ( # fixed size tensor + scalar -> fixed size tensor
+        """
+#trait_add = {{
+ indexing_maps = [
+   affine_map<(i, j) -> (i, j)>,
+   affine_map<(i, j) -> (i, j)>
+ ],
+ iterator_types = [\"parallel\", \"parallel\"],
+ doc = \" !!! DUMMY DOCUMENTATION !!! \"
+}}
+func @{func_name}(%arg_tensor: tensor<2x4x{mlir_type}>, %arg_scalar: {mlir_type}) -> tensor<2x4x{mlir_type}> {{
+ %answer = linalg.generic #trait_add
+    ins(%arg_tensor: tensor<2x4x{mlir_type}>)
+   outs(%arg_tensor: tensor<2x4x{mlir_type}>) {{
+     ^bb(%a: {mlir_type}, %s: {mlir_type}):
+       %sum = {linalg_add} %a, %arg_scalar : {mlir_type}
+       linalg.yield %sum : {mlir_type}
+ }} -> tensor<2x4x{mlir_type}>
+ return %answer : tensor<2x4x{mlir_type}>
+}}
+""",
+        [
+            np.arange(2*4).reshape((2,4)),
+            2
+        ],
+        np.array([
+            [2, 3, 4, 5],
+            [6, 7, 8, 9]
+        ])
+    ),
+    
+    ( # scalar + fixed size tensor -> fixed size tensor
+        """
+#trait_add = {{
+ indexing_maps = [
+   affine_map<(i, j) -> (i, j)>,
+   affine_map<(i, j) -> (i, j)>
+ ],
+ iterator_types = [\"parallel\", \"parallel\"],
+ doc = \" !!! DUMMY DOCUMENTATION !!! \"
+}}
+func @{func_name}(%arg_scalar: {mlir_type}, %arg_tensor: tensor<2x4x{mlir_type}>) -> tensor<2x4x{mlir_type}> {{
+ %answer = linalg.generic #trait_add
+    ins(%arg_tensor: tensor<2x4x{mlir_type}>)
+   outs(%arg_tensor: tensor<2x4x{mlir_type}>) {{
+     ^bb(%a: {mlir_type}, %s: {mlir_type}):
+       %sum = {linalg_add} %a, %arg_scalar : {mlir_type}
+       linalg.yield %sum : {mlir_type}
+ }} -> tensor<2x4x{mlir_type}>
+ return %answer : tensor<2x4x{mlir_type}>
+}}
+""",
+        [
+            2,
+            np.arange(2*4).reshape((2,4))
+        ],
+        np.array([
+            [2, 3, 4, 5],
+            [6, 7, 8, 9]
+        ])
+    ),
+
+    
+    ( # fixed size tensor + scalar + fixed size tensor -> fixed size tensor
+        """
+#trait_add = {{
+ indexing_maps = [
+   affine_map<(i, j) -> (i, j)>,
+   affine_map<(i, j) -> (i, j)>
+ ],
+ iterator_types = [\"parallel\", \"parallel\"],
+ doc = \" !!! DUMMY DOCUMENTATION !!! \"
+}}
+func @{func_name}(%tensor_a: tensor<2x4x{mlir_type}>, %arg_scalar: {mlir_type}, %tensor_b: tensor<2x4x{mlir_type}>) -> tensor<2x4x{mlir_type}> {{
+ %answer = linalg.generic #trait_add
+    ins(%tensor_a, %tensor_b: tensor<2x4x{mlir_type}>, tensor<2x4x{mlir_type}>)
+   outs(%tensor_a: tensor<2x4x{mlir_type}>) {{
+     ^bb(%a: {mlir_type}, %b: {mlir_type}, %s: {mlir_type}):
+       %ab = {linalg_add} %a, %b : {mlir_type}
+       %sum = {linalg_add} %ab, %arg_scalar : {mlir_type}
+       linalg.yield %sum : {mlir_type}
+ }} -> tensor<2x4x{mlir_type}>
+ return %answer : tensor<2x4x{mlir_type}>
+}}
+""",
+        [
+            np.arange(2*4).reshape((2,4)),
+            2,
+            np.full([2,4], 10)
+        ],
+        np.array([
+            [12, 13, 14, 15],
+            [16, 17, 18, 19]
+        ])
+    ),
+]
+
+#########
+# Tests #
+#########
+
+def test_jit_engine():
+    
+    engine = mlir_graphblas.MlirJitEngine()
+    for test_case_index, test_case in enumerate(TEST_CASES):
+        for mlir_type, np_type in MLIR_TYPE_TO_NP_TYPE.items():
+            func_name = f"func_{test_case_index}_{mlir_type}"
+            
+            if issubclass(np_type, np.integer):
+                linalg_add = 'addi'
+            elif issubclass(np_type, np.floating):
+                linalg_add = 'addf'
+            else:
+                raise ValueError(f"No MLIR type for {np_type}.")
+            
+            mlir_template, args, expected_result = test_case
+            mlir_text = mlir_template.format(
+                func_name=func_name,
+                mlir_type=mlir_type,
+                linalg_add=linalg_add
+            )
+            args =  [arg.astype(np_type) if isinstance(arg, np.ndarray) else arg for arg in args]
+            expected_result =  expected_result.astype(np_type) if isinstance(expected_result, np.ndarray) else expected_result
+            
+            engine.add(mlir_text, STANDARD_PASSES)
+        
+            compiled_func = engine[func_name]
+            result = compiled_func(*args)
+            assert np.all(result == expected_result), f"""
+Input MLIR: 
+{mlir_text}
+
+Inputs: {args}
+Result: {result}
+Expected Result: {expected_result}
+"""
