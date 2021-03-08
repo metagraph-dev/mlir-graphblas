e0a35703f0c9184a820cda181495b0b053874df1
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-03-08 07:36:36 -0800

    Refactor tests
    This commit refactors the tests.
    
    This also adds JIT engine test cases for situations where MLIR functions return a scalar.

---
+++ mlir_graphblas/tests/jit_engine_test_utils.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+
+STANDARD_PASSES = [
+    '--linalg-bufferize',
+    '--func-bufferize',
+    '--tensor-bufferize',
+    '--tensor-constant-bufferize',
+    '--finalizing-bufferize',
+    '--convert-linalg-to-loops',
+    '--convert-scf-to-std',
+    '--convert-std-to-llvm',
+]
+
+MLIR_TYPE_TO_NP_TYPE = {
+    'i8': np.int8,
+    'i16': np.int16,
+    'i32': np.int32,
+    'i64': np.int64,
+    # 'f16': np.float16, # 16-bit floats don't seem to be supported in ctypes
+    'f32': np.float32,
+    'f64': np.float64,
+}
