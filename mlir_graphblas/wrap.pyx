51d4cc5b3747d57b649cffba4126dbb4e2c7728a
Erik Welch <erik.n.welch@gmail.com>
2021-03-10 16:14:02 -0600

    Clean up cython code
    - Check dimensions of arrays
    - Use memoryview Cython types in function signature (preferred over numpy API if possible)
    - free/delete C++ objects when we're finished with them
    - Move example to `run_example` function

--- mlir_graphblas/wrap.pyx
+++ mlir_graphblas/wrap.pyx
@@ -1,5 +1,8 @@
+cimport cython
+import numpy as np
+cimport numpy as np
 from libc.stdint cimport uint64_t, uintptr_t
-from libc.stdlib cimport malloc
+from libc.stdlib cimport malloc, free
 from libcpp.vector cimport vector
 from libcpp cimport bool
 
@@ -33,47 +36,86 @@ def myfunc():
     del st
 
 
-def build_sparse_tensor(): #indices, values, sizes, sparsity):
-    # R: rank, number of dimensions
-    # N: number of values
-    # indices[N, R]
-    # values[N]
-    # sparsity[R]
-    # sizes[R]
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef uintptr_t build_sparse_tensor(
+    uint64_t[:, :] indices, # N x D
+    np.float64_t[:] values, # N
+    uint64_t[:] sizes,      # D
+    bool[:] sparsity,       # D
+) except 0:
+    cdef np.intp_t N = values.shape[0]  # number of values
+    cdef np.intp_t D = sizes.shape[0]  # rank, number of dimensions
+    if sparsity.shape[0] != D:
+        raise ValueError(
+            f'Length of sizes and sparsity arrays must match: {sizes.shape[0]} != {sparsity.shape[0]}'
+        )
+    if indices.shape[0] != N:
+        raise ValueError(
+            'First dimension of indices array must match length of values array: '
+            f'{indices.shape[0]} != {values.shape[0]}'
+        )
+    if indices.shape[1] != D:
+        raise ValueError(
+            'Second dimension of indices array must match length of sparsity and sizes arrays: '
+            f'{indices.shape[1]} != {D}'
+        )
 
-    import numpy as np
-    indices = np.array([[0, 0], [1, 1]])
-    values = np.array([1.2, 3.4])
-    sizes = np.array([2, 2])
-    sparsity = np.array([True, True])
-
-    cdef uint64_t N = values.size
-    cdef uint64_t R = sizes.size
-    cdef vector[uint64_t] sizes_vector = vector[uint64_t](R)
-    for i in range(R):
+    cdef vector[uint64_t] sizes_vector = vector[uint64_t](D)
+    for i in range(D):
         sizes_vector[i] = sizes[i]
     cdef SparseTensor *tensor = new SparseTensor(sizes_vector, N)
-    print('tensor rank', tensor.getRank())
 
-    cdef vector[vector[uint64_t]] indices_vector = vector[vector[uint64_t]](N)
-    cdef vector[uint64_t] * ind_ptr
-    for i in range(N):
-        ind_ptr = new vector[uint64_t](R)
-        indices_vector[i] = ind_ptr[0]
-
+    cdef vector[uint64_t] *ind_ptr
     cdef vector[uint64_t] ind
     for i in range(N):
-        ind = indices_vector[i]
-        for j in range(R):
+        ind_ptr = new vector[uint64_t](D)
+        ind = ind_ptr[0]
+        for j in range(D):
             ind[j] = indices[i, j]
         tensor.add(ind, values[i])
 
-    cdef bool *sparsity_array = <bool*>malloc(sizeof(bool) * R)
-    for i in range(R):
+    cdef bool *sparsity_array = <bool*>malloc(sizeof(bool) * D)
+    for i in range(D):
         sparsity_array[i] = sparsity[i]
-    cdef SparseTensorStorage[uint64_t, uint64_t, double] *rv = new SparseTensorStorage[uint64_t, uint64_t, double](tensor, sparsity_array)
-    print('rank', rv.getRank())
-    print('dim sizes', rv.getDimSize(0), rv.getDimSize(1))
-    print(<uintptr_t>rv)
+    cdef SparseTensorStorage[uint64_t, uint64_t, double] *rv = (
+        new SparseTensorStorage[uint64_t, uint64_t, double](tensor, sparsity_array)
+    )
+    free(sparsity_array)
+    del tensor
     return <uintptr_t>rv
+
+
+def run_example():
+    cdef np.ndarray[uint64_t, ndim=2] indices = np.array([[0, 0], [1, 1]], dtype=np.uint64)
+    cdef np.ndarray[np.float64_t, ndim=1] values = np.array([1.2, 3.4], dtype=np.float64)
+    cdef np.ndarray[uint64_t, ndim=1] sizes = np.array([2, 2], dtype=np.uint64)
+    cdef np.ndarray[bool, ndim=1] sparsity = np.array([True, True], dtype=np.bool8)
+    cdef uintptr_t ptr = build_sparse_tensor(indices, values, sizes, sparsity)
+    cdef SparseTensorStorage[uint64_t, uint64_t, double] *tensor = (
+        <SparseTensorStorage[uint64_t, uint64_t, double]*>ptr
+    )
+    print('rank:', tensor.getRank())
+    print('dim sizes:', tensor.getDimSize(0), tensor.getDimSize(1))
+    print('pointer:', ptr)
+
+    cdef vector[uint64_t] *ind_ptr
+    print('pointers')
+    tensor.getPointers(&ind_ptr, 0)
+    print('  0:', ind_ptr[0])
+    tensor.getPointers(&ind_ptr, 1)
+    print('  1:', ind_ptr[0])
+
+    print('indices')
+    tensor.getIndices(&ind_ptr, 0)
+    print('  0:', ind_ptr[0])
+    tensor.getIndices(&ind_ptr, 1)
+    print('  1:', ind_ptr[0])
+
+    print('values')
+    cdef vector[double] *val_ptr
+    tensor.getValues(&val_ptr)
+    print(' ', val_ptr[0])
+
+    return ptr
 
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import numpy as np
 from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 from Cython.Compiler.Options import get_directive_defaults
@@ -6,6 +7,10 @@ import versioneer
 directive_defaults = get_directive_defaults()
 directive_defaults["binding"] = True
 directive_defaults["language_level"] = 3
+
+include_dirs = [np.get_include()]
+define_macros = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
+annotate = True  # Creates html file
 
 setup(
     name="mlir-graphblas",
@@ -20,7 +25,10 @@ setup(
             language='c++',
             sources=["mlir_graphblas/wrap.pyx"],
             extra_compile_args=["-std=c++11"],
-        )
+            include_dirs=include_dirs,
+            define_macros=define_macros,
+        ),
+        annotate=annotate,
     ),
     package_data={"mlir_graphblas": ["*.pyx"]},
     include_package_data=True,
