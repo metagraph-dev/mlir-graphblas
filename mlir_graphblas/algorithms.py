5d1a73c1044cd316ca897d6e5e58fafac73bb55b
Jim Kitchen <jim22k@gmail.com>
2021-05-05 12:40:11 -0500

    Move creation of new matrices inside the MLIR text

--- mlir_graphblas/algorithms.py
+++ mlir_graphblas/algorithms.py
@@ -1,6 +1,5 @@
 from typing import List
 from .functions import (
-    create_empty_matrix,
     Transpose,
     MatrixSelect,
     MatrixReduceToScalar,
@@ -10,7 +9,7 @@ from .functions import (
 from .sparse_utils import MLIRSparseTensor
 
 
-transpose = Transpose().compile()
+csr_to_csc = Transpose(swap_sizes=False).compile()
 matrix_select_triu = MatrixSelect("TRIU").compile()
 matrix_select_tril = MatrixSelect("TRIL").compile()
 matrix_select_gt0 = MatrixSelect("gt0").compile()
@@ -23,15 +22,11 @@ mxm_plus_plus = MatrixMultiply("plus_plus").compile()
 
 def triangle_count(A: MLIRSparseTensor) -> int:
     # Create U and L matrices
-    U = create_empty_matrix(*A.shape, nnz=A.pointers[1][-1])
-    L = create_empty_matrix(*A.shape, nnz=A.pointers[1][-1])
-    matrix_select_triu(U, A)
-    matrix_select_tril(L, A)
+    U = matrix_select_triu(A)
+    L = matrix_select_tril(A)
     # Count Triangles
-    UT = create_empty_matrix(*U.shape, nnz=U.pointers[1][-1])
-    transpose(UT, U)
-    C = create_empty_matrix(*A.shape, nnz=L.pointers[1][-1])
-    mxm_plus_pair(C, L, UT, L)
+    U_csc = csr_to_csc(U)
+    C = mxm_plus_pair(L, U_csc, L)
     num_triangles = matrix_reduce(C)
     assert (
         int(num_triangles) == num_triangles
@@ -45,24 +40,25 @@ def dense_neural_network(
     Y0: MLIRSparseTensor,
     ymax=32.0,
 ):
+    import time
+
     nlayers = len(W)
 
-    # We will swap between Y1 and Y2 in each calculation
-    Y1 = create_empty_matrix(*Y0.shape, nnz=int(Y0.pointers[1][-1]) * 10)
-    Y2 = create_empty_matrix(*Y0.shape, nnz=int(Y0.pointers[1][-1]) * 10)
-    WT = create_empty_matrix(*W[0].shape, nnz=W[0].pointers[1][-1])
+    Y = Y0.dup()
+    very_start = now = time.time()
     for layer in range(nlayers):
-        print(f"Layer {layer+1} of {nlayers}")
-        transpose(WT, W[layer])
-        if layer == 0:
-            mxm_plus_times(Y1, Y0, WT)
-        else:
-            mxm_plus_times(Y1, Y2, WT)
+        W_csc = csr_to_csc(W[layer])
+        Y = mxm_plus_times(Y, W_csc)
 
         # Normally, I would need to transpose this, but I know these are purely diagonal matrices
-        mxm_plus_plus(Y2, Y1, Bias[layer])
+        Y = mxm_plus_plus(Y, Bias[layer])
 
-        matrix_select_gt0(Y1, Y2)
-        matrix_apply_min(Y2, Y1, ymax)
+        Y = matrix_select_gt0(Y)
+        Y = matrix_apply_min(Y, ymax)
 
-    return Y2
+        curr = time.time()
+        diff, now = curr - now, curr
+        print(f"Layer {layer+1} of {nlayers} took {diff:.2f} sec")
+
+    print(f"\nTotal time = {(now - very_start):.2f} sec")
+    return Y
