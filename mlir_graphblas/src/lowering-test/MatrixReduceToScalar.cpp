eab5e18245c453379bc12f36d6938a9f331bba41
Stan Seibert <sseibert@anaconda.com>
2021-05-24 11:46:30 -0500

    get sparse tensor components

--- mlir_graphblas/src/lowering-test/MatrixReduceToScalar.cpp
+++ mlir_graphblas/src/lowering-test/MatrixReduceToScalar.cpp
@@ -2,8 +2,10 @@
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/BuiltinTypes.h"
+#include "mlir/Dialect/LLVMIR/LLVMTypes.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/SCF/SCF.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/Verifier.h"
 #include "mlir/Dialect/SparseTensor/IR/SparseTensor.h"
@@ -37,24 +39,35 @@ void addMatrixReduceToScalarFunc(mlir::ModuleOp mod, const std::string &aggregat
     auto &entry_block = *func.addEntryBlock();
     builder.setInsertionPointToStart(&entry_block);
 
+    auto input = entry_block.getArgument(0);
+
     // Initial constants
+    auto int64Type = builder.getIntegerType(64);
     auto cf0 = builder.create<ConstantFloatOp>(unLoc, APFloat(0.0), valueType);
     auto c0 = builder.create<ConstantIndexOp>(unLoc, 0);
     auto c1 = builder.create<ConstantIndexOp>(unLoc, 1);
+    auto c0_64 = builder.create<ConstantIntOp>(unLoc, 0, int64Type);
+    auto c1_64 = builder.create<ConstantIntOp>(unLoc, 1, int64Type);
 
     // Get sparse tensor info
-    //auto nrows = builder.create<CallOp>()
-    /*         %0 = call @sparseDimSize(%input, %c0) : (!llvm.ptr<i8>, index) -> index
-        %1 = call @sparsePointers64(%input, %c1) : (!llvm.ptr<i8>, index) -> memref<?xindex>
-        %2 = call @sparseIndices64(%input, %c1) : (!llvm.ptr<i8>, index) -> memref<?xindex>
-        %3 = call @sparseValuesF64(%input) : (!llvm.ptr<i8>) -> memref<?xf64>
-        */
-    // TBD
-    
+    auto memref1DI64Type = MemRefType::get({-1}, int64Type);
+    auto memref1DValueType = MemRefType::get({-1}, valueType);
+
+    auto nrows = builder.create<memref::DimOp>(unLoc, input, c0.getResult());
+    auto inputPtrs = builder.create<ToPointersOp>(unLoc, memref1DI64Type, input, c1.getResult());
+    auto inputIndices = builder.create<ToIndicesOp>(unLoc, memref1DI64Type, input, c1.getResult());
+    auto inputValues = builder.create<ToValuesOp>(unLoc, memref1DValueType, input);
+
     // Allocate temporary storage
     auto memrefF64 = MemRefType::get({}, valueType);
     auto acc = builder.create<memref::AllocOp>(unLoc, memrefF64);
     builder.create<memref::StoreOp>(unLoc, cf0, acc);
+
+    // outer for loop
+    // scf.for %arg1 = %c0 to %0 step %c1 {
+    auto loop = builder.create<scf::ForOp>(unLoc, c0, nrows, c1);
+
+
 
     // Add return op
     builder.create<ReturnOp>(builder.getUnknownLoc());
