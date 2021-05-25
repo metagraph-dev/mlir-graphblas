7ba80be82b8489f517dc77d27f3b82a406c3872e
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-05-25 16:25:46 -0500

    Implement initial scaffolding for a graphblas-opt pass.

---
+++ mlir_graphblas/src/include/GraphBLAS/GraphBLASPasses.h
@@ -0,0 +1,26 @@
+//===- GraphBLASPasses.h - GraphBLAS dialect passes -----------------*- C++ -*-===//
+//
+// TODO add documentation
+//
+//===--------------------------------------------------------------------------===//
+
+#ifndef GRAPHBLAS_GRAPHBLASPASSES_H
+#define GRAPHBLAS_GRAPHBLASPASSES_H
+
+#include "mlir/Pass/Pass.h"
+
+namespace mlir {
+
+std::unique_ptr<OperationPass<FuncOp>> createGraphBLASLowerMatrixMultiplyPass();
+
+}
+
+//===----------------------------------------------------------------------===//
+// Registration.
+//===----------------------------------------------------------------------===//
+
+// Generate the code for registering passes.
+#define GEN_PASS_REGISTRATION
+#include "GraphBLAS/GraphBLASPasses.h.inc"
+
+#endif // GRAPHBLAS_GRAPHBLASPASSES_H
