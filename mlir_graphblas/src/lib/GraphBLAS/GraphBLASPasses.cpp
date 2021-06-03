c713173dc45b267caef3ec92a810de37b712bdfc
Stan Seibert <sseibert@anaconda.com>
2021-06-02 19:22:12 -0500

    Lowering transpose operator

--- mlir_graphblas/src/lib/GraphBLAS/GraphBLASPasses.cpp
+++ mlir_graphblas/src/lib/GraphBLAS/GraphBLASPasses.cpp
@@ -15,30 +15,11 @@
 #include "llvm/ADT/None.h"
 
 #include "GraphBLAS/GraphBLASPasses.h"
+#include "GraphBLAS/GraphBLASUtils.h"
 
 using namespace ::mlir;
 
 namespace {
-
-//===----------------------------------------------------------------------===//
-// Passes Implementation Helpers.
-//===----------------------------------------------------------------------===//
-
-mlir::RankedTensorType getCSRTensorType(MLIRContext *context, Type valueType) {
-    SmallVector<sparse_tensor::SparseTensorEncodingAttr::DimLevelType, 2> dlt;
-    dlt.push_back(sparse_tensor::SparseTensorEncodingAttr::DimLevelType::Dense);
-    dlt.push_back(sparse_tensor::SparseTensorEncodingAttr::DimLevelType::Compressed);
-    unsigned ptr = 64;
-    unsigned ind = 64;
-    AffineMap map = AffineMap::getMultiDimIdentityMap(2, context);
-
-    RankedTensorType csrTensor = RankedTensorType::get(
-        {-1, -1}, /* 2D, unknown size */
-        valueType,
-        sparse_tensor::SparseTensorEncodingAttr::get(context, dlt, map, ptr, ind));
-
-    return csrTensor;
-}
 
 //===----------------------------------------------------------------------===//
 // Passes declaration.
@@ -54,8 +35,146 @@ mlir::RankedTensorType getCSRTensorType(MLIRContext *context, Type valueType) {
 class LowerTransposeRewrite : public OpRewritePattern<graphblas::TransposeOp> {
 public:
   using OpRewritePattern<graphblas::TransposeOp>::OpRewritePattern;
-  LogicalResult matchAndRewrite(graphblas::TransposeOp op, PatternRewriter &rewriter) const {    
-    return failure();
+  LogicalResult matchAndRewrite(graphblas::TransposeOp op, PatternRewriter &rewriter) const {
+    ModuleOp module = op->getParentOfType<ModuleOp>();
+    Location loc = op->getLoc();
+
+    ValueTypeRange<OperandRange> operandTypes = op->getOperandTypes();
+    Value inputTensor = op.input();
+    Type valueType = operandTypes.front().dyn_cast<TensorType>().getElementType();
+    Type int64Type = rewriter.getIntegerType(64);
+    Type indexType = rewriter.getIndexType();
+
+    // Initial constants
+    Value c0 = rewriter.create<ConstantIndexOp>(loc, 0);
+    Value c1 = rewriter.create<ConstantIndexOp>(loc, 1);
+    Value c0_64 = rewriter.create<ConstantIntOp>(loc, 0, int64Type);
+    Value c1_64 = rewriter.create<ConstantIntOp>(loc, 1, int64Type);
+
+    // Get sparse tensor info
+    auto memref1DI64Type = MemRefType::get({-1}, int64Type);
+    auto memref1DValueType = MemRefType::get({-1}, valueType);
+
+    auto inputPtrs = rewriter.create<sparse_tensor::ToPointersOp>(loc, memref1DI64Type, inputTensor, c1);
+    auto inputIndices = rewriter.create<sparse_tensor::ToIndicesOp>(loc, memref1DI64Type, inputTensor, c1);
+    auto inputValues = rewriter.create<sparse_tensor::ToValuesOp>(loc, memref1DValueType, inputTensor);
+    Value nrow = rewriter.create<memref::DimOp>(loc, inputTensor, c0);
+    Value ncol = rewriter.create<memref::DimOp>(loc, inputTensor, c1);
+    Value ncols_plus_one = rewriter.create<mlir::AddIOp>(loc, ncol, c1);
+
+    Value nnz_64 = rewriter.create<memref::LoadOp>(loc, inputPtrs, nrow);
+    Value nnz = rewriter.create<mlir::IndexCastOp>(loc, nnz_64, indexType);
+
+    auto output = callEmptyLike(rewriter, module, loc, inputTensor).getResult(0);
+    bool swap_sizes = op->getAttr("swap_sizes").dyn_cast<BoolAttr>().getValue();
+    if (swap_sizes)
+    {
+      callResizeDim(rewriter, module, loc, output, c0, ncol);
+      callResizeDim(rewriter, module, loc, output, c1, nrow);
+    }
+    else
+    {
+      callResizeDim(rewriter, module, loc, output, c0, nrow);
+      callResizeDim(rewriter, module, loc, output, c1, ncol);
+    }
+
+    callResizePointers(rewriter, module, loc, output, c1, ncols_plus_one);
+    callResizeIndex(rewriter, module, loc, output, c1, nnz);
+    callResizeValues(rewriter, module, loc, output, nnz);
+
+    auto outputPtrs = rewriter.create<sparse_tensor::ToPointersOp>(loc, memref1DI64Type, output, c1);
+    auto outputIndices = rewriter.create<sparse_tensor::ToIndicesOp>(loc, memref1DI64Type, output, c1);
+    auto outputValues = rewriter.create<sparse_tensor::ToValuesOp>(loc, memref1DValueType, output);
+
+    // compute number of non-zero entries per column of A
+
+    // init B.pointers to zero
+    auto initLoop = rewriter.create<scf::ForOp>(loc, c0, ncol, c1);
+    auto initLoopIdx = initLoop.getInductionVar();
+    rewriter.setInsertionPointToStart(initLoop.getBody());
+    rewriter.create<memref::StoreOp>(loc, c0_64, outputPtrs, initLoopIdx);
+    rewriter.setInsertionPointAfter(initLoop);
+
+    // store pointers
+    auto ptrLoop = rewriter.create<scf::ForOp>(loc, c0, nnz, c1);
+    auto ptrLoopIdx = ptrLoop.getInductionVar();
+
+    rewriter.setInsertionPointToStart(ptrLoop.getBody());
+    Value colA64 = rewriter.create<memref::LoadOp>(loc, inputIndices, ptrLoopIdx);
+    Value colA = rewriter.create<mlir::IndexCastOp>(loc, colA64, indexType);
+    Value colB = rewriter.create<memref::LoadOp>(loc, outputPtrs, colA);
+    Value colB1 = rewriter.create<mlir::AddIOp>(loc, colB, c1_64);
+    rewriter.create<memref::StoreOp>(loc, colB1, outputPtrs, colA);
+
+    rewriter.setInsertionPointAfter(ptrLoop);
+
+    // cumsum the nnz per column to get Bp
+    rewriter.create<memref::StoreOp>(loc, c0_64, outputPtrs, ncol);
+
+    auto colAccLoop = rewriter.create<scf::ForOp>(loc, c0, ncol, c1);
+    auto colAccLoopIdx = colAccLoop.getInductionVar();
+
+    rewriter.setInsertionPointToStart(colAccLoop.getBody());
+    Value temp = rewriter.create<memref::LoadOp>(loc, outputPtrs, colAccLoopIdx);
+    Value cumsum = rewriter.create<memref::LoadOp>(loc, outputPtrs, ncol);
+    rewriter.create<memref::StoreOp>(loc, cumsum, outputPtrs, colAccLoopIdx);
+    Value cumsum2 = rewriter.create<mlir::AddIOp>(loc, cumsum, temp);
+    rewriter.create<memref::StoreOp>(loc, cumsum2, outputPtrs, ncol);
+
+    rewriter.setInsertionPointAfter(colAccLoop);
+
+    // copy values
+    auto outerLoop = rewriter.create<scf::ForOp>(loc, c0, nrow, c1);
+    Value rowIdx = outerLoop.getInductionVar();
+
+    rewriter.setInsertionPointToStart(outerLoop.getBody());
+    Value row_64 = rewriter.create<mlir::IndexCastOp>(loc, rowIdx, int64Type);
+    Value j_start_64 = rewriter.create<memref::LoadOp>(loc, inputPtrs, rowIdx);
+    Value j_start = rewriter.create<mlir::IndexCastOp>(loc, j_start_64, indexType);
+    Value row_plus1 = rewriter.create<mlir::AddIOp>(loc, rowIdx, c1);
+    Value j_end_64 = rewriter.create<memref::LoadOp>(loc, inputPtrs, row_plus1);
+    Value j_end = rewriter.create<mlir::IndexCastOp>(loc, j_end_64, indexType);
+
+    auto innerLoop = rewriter.create<scf::ForOp>(loc, j_start, j_end, c1);
+    Value jj = innerLoop.getInductionVar();
+
+    rewriter.setInsertionPointToStart(innerLoop.getBody());
+
+    Value col_64 = rewriter.create<memref::LoadOp>(loc, inputIndices, jj);
+    Value col = rewriter.create<mlir::IndexCastOp>(loc, col_64, indexType);
+    Value dest_64 = rewriter.create<memref::LoadOp>(loc, outputPtrs, col);
+    Value dest = rewriter.create<mlir::IndexCastOp>(loc, dest_64, indexType);
+    rewriter.create<memref::StoreOp>(loc, row_64, outputIndices, dest);
+    Value axjj = rewriter.create<memref::LoadOp>(loc, inputValues, jj);
+    rewriter.create<memref::StoreOp>(loc, axjj, outputValues, dest);
+
+    // Bp[col]++
+    Value bp_inc = rewriter.create<memref::LoadOp>(loc, outputPtrs, col);
+    Value bp_inc1 = rewriter.create<mlir::AddIOp>(loc, bp_inc, c1_64);
+    rewriter.create<memref::StoreOp>(loc, bp_inc1, outputPtrs, col);
+
+    rewriter.setInsertionPointAfter(outerLoop);
+
+    Value last_last = rewriter.create<memref::LoadOp>(loc, outputPtrs, ncol);
+    rewriter.create<memref::StoreOp>(loc, c0_64, outputPtrs, ncol);
+
+    auto finalLoop = rewriter.create<scf::ForOp>(loc, c0, ncol, c1);
+    Value iCol = finalLoop.getInductionVar();
+
+    rewriter.setInsertionPointToStart(finalLoop.getBody());
+
+    Value swapTemp = rewriter.create<memref::LoadOp>(loc, outputPtrs, iCol);
+    Value last = rewriter.create<memref::LoadOp>(loc, outputPtrs, ncol);
+    rewriter.create<memref::StoreOp>(loc, last, outputPtrs, iCol);
+    rewriter.create<memref::StoreOp>(loc, swapTemp, outputPtrs, ncol);
+
+    rewriter.setInsertionPointAfter(finalLoop);
+
+    rewriter.create<memref::StoreOp>(loc, last_last, outputPtrs, ncol);
+
+    rewriter.replaceOp(op, output);
+
+    return success();
   };
 };
 
@@ -225,7 +344,8 @@ public:
 void populateGraphBLASLoweringPatterns(RewritePatternSet &patterns) {
   patterns.add<
     LowerMatrixReduceToScalarRewrite,
-    LowerMatrixMultiplyRewrite
+    LowerMatrixMultiplyRewrite,
+    LowerTransposeRewrite
     >(patterns.getContext());
 }
 
---
+++ mlir_graphblas/src/lib/GraphBLAS/GraphBLASUtils.cpp
@@ -0,0 +1,112 @@
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "mlir/Dialect/SparseTensor/IR/SparseTensor.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+
+using namespace std;
+using namespace mlir;
+using namespace mlir::sparse_tensor;
+
+// make CSR tensor type
+mlir::RankedTensorType getCSRTensorType(MLIRContext *context, Type valueType)
+{
+    SmallVector<sparse_tensor::SparseTensorEncodingAttr::DimLevelType, 2> dlt;
+    dlt.push_back(sparse_tensor::SparseTensorEncodingAttr::DimLevelType::Dense);
+    dlt.push_back(sparse_tensor::SparseTensorEncodingAttr::DimLevelType::Compressed);
+    unsigned ptr = 64;
+    unsigned ind = 64;
+    AffineMap map = AffineMap::getMultiDimIdentityMap(2, context);
+
+    RankedTensorType csrTensor = RankedTensorType::get(
+        {-1, -1}, /* 2D, unknown size */
+        valueType,
+        sparse_tensor::SparseTensorEncodingAttr::get(context, dlt, map, ptr, ind));
+
+    return csrTensor;
+}
+
+/// Returns function reference (first hit also inserts into module).
+// from: llvm/llvm-project/mlir/lib/Dialect/SparseTensor/Transforms/SparseTensorConversion.cpp
+static FlatSymbolRefAttr getFunc(mlir::ModuleOp &mod, mlir::Location &loc, StringRef name, TypeRange result,
+                                 TypeRange operands)
+{
+    MLIRContext *context = mod.getContext();
+    auto func = mod.lookupSymbol<FuncOp>(name);
+    if (!func)
+    {
+        OpBuilder moduleBuilder(mod.getBodyRegion());
+        moduleBuilder
+            .create<FuncOp>(loc, name,
+                            FunctionType::get(context, operands, result))
+            .setPrivate();
+    }
+    return SymbolRefAttr::get(context, name);
+}
+
+mlir::CallOp callEmptyLike(mlir::OpBuilder &builder, mlir::ModuleOp &mod, mlir::Location loc, mlir::Value tensor) {
+    auto tensorType = tensor.getType();
+
+    auto func = getFunc(mod, loc, "empty_like", tensorType, tensorType);
+    auto result = builder.create<mlir::CallOp>(loc, func, tensorType, tensor);
+
+    return result;
+}
+
+mlir::CallOp callDupTensor(mlir::OpBuilder &builder, mlir::ModuleOp &mod, mlir::Location loc, mlir::Value tensor) {
+    auto tensorType = tensor.getType();
+
+    auto func = getFunc(mod, loc, "dup_tensor", tensorType, tensorType);
+    auto result = builder.create<mlir::CallOp>(loc, func, tensorType, tensor);
+
+    return result;
+}
+
+mlir::CallOp callResizeDim(mlir::OpBuilder &builder, mlir::ModuleOp &mod, mlir::Location loc,
+                           mlir::Value tensor, mlir::Value d, mlir::Value size)
+{
+    auto tensorType = tensor.getType();
+
+    auto indexType = builder.getIndexType();
+    auto func = getFunc(mod, loc, "resize_dim", TypeRange(), {tensorType, indexType, indexType});
+    auto result = builder.create<mlir::CallOp>(loc, func, TypeRange(), ArrayRef<Value>({tensor, d, size}));
+
+    return result;
+}
+
+mlir::CallOp callResizePointers(mlir::OpBuilder &builder, mlir::ModuleOp &mod, mlir::Location loc,
+                                mlir::Value tensor, mlir::Value d, mlir::Value size)
+{
+    auto tensorType = tensor.getType();
+
+    auto indexType = builder.getIndexType();
+    auto func = getFunc(mod, loc, "resize_pointers", TypeRange(), {tensorType, indexType, indexType});
+    auto result = builder.create<mlir::CallOp>(loc, func, TypeRange(), ArrayRef<Value>({tensor, d, size}));
+
+    return result;
+}
+
+mlir::CallOp callResizeIndex(mlir::OpBuilder &builder, mlir::ModuleOp &mod, mlir::Location loc,
+                                mlir::Value tensor, mlir::Value d, mlir::Value size)
+{
+    auto tensorType = tensor.getType();
+
+    auto indexType = builder.getIndexType();
+    auto func = getFunc(mod, loc, "resize_index", TypeRange(), {tensorType, indexType, indexType});
+    auto result = builder.create<mlir::CallOp>(loc, func, TypeRange(), ArrayRef<Value>({tensor, d, size}));
+
+    return result;
+}
+
+mlir::CallOp callResizeValues(mlir::OpBuilder &builder, mlir::ModuleOp &mod, mlir::Location loc,
+                              mlir::Value tensor, mlir::Value size)
+{
+    auto tensorType = tensor.getType();
+
+    auto indexType = builder.getIndexType();
+    auto func = getFunc(mod, loc, "resize_values", TypeRange(), {tensorType, indexType});
+    auto result = builder.create<mlir::CallOp>(loc, func, TypeRange(), ArrayRef<Value>({tensor, size}));
+
+    return result;
+}
