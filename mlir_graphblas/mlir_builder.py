1ed7277177e66fe1cd5cc3571f85ab9fe5a4cc80
Stan Seibert <sseibert@anaconda.com>
2021-10-21 13:59:01 -0500

    fix final issues

--- mlir_graphblas/mlir_builder.py
+++ mlir_graphblas/mlir_builder.py
@@ -46,8 +46,8 @@ GRAPHBLAS_TO_SCF_PASSES = (
 SCF_TO_LLVM_PASSES = (
     "--convert-scf-to-std",
     "--convert-memref-to-llvm",
-    "--convert-openmp-to-llvm",
     "--convert-math-to-llvm",
+    "--convert-openmp-to-llvm",
     "--convert-arith-to-llvm",
     "--convert-std-to-llvm",
     "--reconcile-unrealized-casts",
