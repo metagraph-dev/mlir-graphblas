ffd97b762dcd82a5770d4b68c671e597b62ff4d5
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2022-02-15 15:48:45 -0600

    Make --graphblas-linalg-lower support graphblas.applly_generic
    Imagine we had the file example.mlir
    
            #accesses = [
              affine_map<(m, n) -> (m, n)>,
              affine_map<(m, n) -> (m, n)>
            ]
    
            #trait = {
              indexing_maps = #accesses,
              iterator_types = ["parallel", "parallel"]
            }
    
            #CSR64 = #sparse_tensor.encoding<{
              dimLevelType = [ "dense", "compressed" ],
              dimOrdering = affine_map<(i,j) -> (i,j)>,
              pointerBitWidth = 64,
              indexBitWidth = 64
            }>
    
            func @main(%A: tensor<?x?xf64, #CSR64>, %thunk: f64) -> tensor<?x?xf64, #CSR64> {
                %answer = graphblas.apply_generic %A : tensor<?x?xf64, #CSR64> to tensor<?x?xf64, #CSR64> {
                  ^bb0(%val: f64):
                    %pick = arith.cmpf olt, %val, %thunk : f64
                    %result = arith.select %pick, %val, %thunk : f64
                    graphblas.yield transform_out %result : f64
                }
                return %answer : tensor<?x?xf64, #CSR64>
            }
    
    We can now do this:
    
            (build_mlir) pnguyen@CONDA-0584:/Users/pnguyen/code/mlir-graphblas$ cat example.mlir | ./mlir_graphblas/src/build/bin/graphblas-opt --graphblas-linalg-lower --print-ir-after-failure | tersify_mlir
            Using development graphblas-opt: /Users/pnguyen/code/mlir-graphblas/mlir_graphblas/src/build/bin/graphblas-opt
            #CSR64 = #sparse_tensor.encoding<{
                dimLevelType = [ "dense", "compressed" ],
                dimOrdering = affine_map<(d0, d1) -> (d0, d1)>,
                pointerBitWidth = 64,
                indexBitWidth = 64
            }>
    
            #map = affine_map<(d0, d1) -> (d1, d0)>
            module {
              func @main(%arg0: tensor<?x?xf64, #CSR64>, %arg1: f64) -> tensor<?x?xf64, #CSR64> {
                %c0 = arith.constant 0 : index
                %c1 = arith.constant 1 : index
                %0 = tensor.dim %arg0, %c0 : tensor<?x?xf64, #CSR64>
                %1 = tensor.dim %arg0, %c1 : tensor<?x?xf64, #CSR64>
                %2 = sparse_tensor.init[%0, %1] : tensor<?x?xf64, #CSR64>
                %3 = linalg.generic {indexing_maps = [#map, #map], iterator_types = ["parallel", "parallel"]} ins(%arg0 : tensor<?x?xf64, #CSR64>) outs(%2 : tensor<?x?xf64, #CSR64>) {
                ^bb0(%arg2: f64, %arg3: f64):
                  %4 = arith.cmpf olt, %arg2, %arg1 : f64
                  %5 = arith.select %4, %arg2, %arg1 : f64
                  linalg.yield %5 : f64
                } -> tensor<?x?xf64, #CSR64>
                return %3 : tensor<?x?xf64, #CSR64>
              }
            }
    
            (build_mlir) pnguyen@CONDA-0584:/Users/pnguyen/code/mlir-graphblas$
    
    NB: The implementation isn't pretty or complete, and there's still some cleaning up to do.

--- mlir_graphblas/src/lib/GraphBLAS/GraphBLASLinalgLowerPass.cpp
+++ mlir_graphblas/src/lib/GraphBLAS/GraphBLASLinalgLowerPass.cpp
@@ -33,7 +33,79 @@ namespace {
 // Passes implementation.
 //===----------------------------------------------------------------------===//
 
-void populateGraphBLASLinalgLoweringPatterns(RewritePatternSet &patterns) {}
+class LinalgLowerApplyGenericRewrite
+    : public OpRewritePattern<graphblas::ApplyGenericOp> {
+public:
+  using OpRewritePattern<graphblas::ApplyGenericOp>::OpRewritePattern;
+  LogicalResult matchAndRewrite(graphblas::ApplyGenericOp op,
+                                PatternRewriter &rewriter) const override {
+
+    MLIRContext *context = op.getContext();
+    ModuleOp module = op->getParentOfType<ModuleOp>();
+    Location loc = op->getLoc();
+
+    Value inputTensor = op.input();
+    RankedTensorType outputTensorType =
+        op.getResult().getType().cast<RankedTensorType>();
+    // bool inPlace = op.in_place();
+
+    Value c0 = rewriter.create<arith::ConstantIndexOp>(loc, 0);
+    Value c1 = rewriter.create<arith::ConstantIndexOp>(loc, 1);
+    Value nrows = rewriter.create<tensor::DimOp>(loc, inputTensor, c0);
+    Value ncols = rewriter.create<tensor::DimOp>(loc, inputTensor, c1);
+
+    Value accumulatorTensor = rewriter.create<sparse_tensor::InitOp>(
+        loc, outputTensorType, ValueRange{nrows, ncols});
+
+    // Required blocks
+    RegionRange extensions = op.extensions();
+    ExtensionBlocks extBlocks;
+    std::set<graphblas::YieldKind> required = {
+        graphblas::YieldKind::TRANSFORM_OUT};
+    LogicalResult extractResult =
+        extBlocks.extractBlocks(op, extensions, required, {});
+
+    if (extractResult.failed()) {
+      return extractResult;
+    }
+
+    graphblas::YieldOp transformOutYield =
+        llvm::dyn_cast_or_null<graphblas::YieldOp>(
+            extBlocks.transformOut->getTerminator());
+
+    // TODO grab these from somewhere else
+    AffineMap map =
+        AffineMap::getPermutationMap(ArrayRef<unsigned>{1, 0}, context);
+    SmallVector<AffineMap, 2> affineMaps = {map, map};
+    SmallVector<StringRef> iteratorTypes = {getParallelIteratorTypeName(),
+                                            getParallelIteratorTypeName()};
+
+    linalg::GenericOp linalgGenericOp = rewriter.create<linalg::GenericOp>(
+        loc, outputTensorType, inputTensor, accumulatorTensor, affineMaps,
+        iteratorTypes,
+        [&](OpBuilder &nestedBuilder, Location nestedLoc,
+            ValueRange blockArgs) {
+          ValueRange arguments = nestedBuilder.getBlock()->getArguments();
+          ValueRange subVals = ValueRange{arguments.front()};
+          rewriter.mergeBlocks(extBlocks.transformOut, nestedBuilder.getBlock(),
+                               subVals);
+          Value result = transformOutYield.values().front();
+          nestedBuilder.create<linalg::YieldOp>(loc, result);
+        });
+    rewriter.eraseOp(transformOutYield);
+    Value output = linalgGenericOp.getResult(0);
+
+    rewriter.replaceOp(op, output);
+
+    cleanupIntermediateTensor(rewriter, module, loc, output);
+
+    return success();
+  };
+};
+
+void populateGraphBLASLinalgLoweringPatterns(RewritePatternSet &patterns) {
+  patterns.add<LinalgLowerApplyGenericRewrite>(patterns.getContext());
+}
 
 struct GraphBLASLinalgLoweringPass
     : public GraphBLASLinalgLoweringBase<GraphBLASLinalgLoweringPass> {
